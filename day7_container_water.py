5608751b5867a15a848ce88bb756ae9ece403529
varun1121k <varun1121kknr@gmail.com>
2026-03-29 17:17:03 +0530

    Create day7_container_water.py

---
+++ day7_container_water.py
@@ -0,0 +1,21 @@
+# Day 7 - Container With Most Water
+
+height = [1,8,6,2,5,4,8,3,7]
+
+left = 0
+right = len(height) - 1
+max_area = 0
+
+while left < right:
+    h = min(height[left], height[right])
+    width = right - left
+    area = h * width
+    
+    max_area = max(max_area, area)
+    
+    if height[left] < height[right]:
+        left += 1
+    else:
+        right -= 1
+
+print("Max area:", max_area)
