bf114793ce3821b50c11b6e205a6eee0b1d19cf6
varun1121k <varun1121kknr@gmail.com>
2026-04-08 11:42:33 +0530

    Create day9_min_subarray.py

---
+++ day9_min_subarray.py
@@ -0,0 +1,21 @@
+# Day 9 - Minimum Size Subarray Sum
+
+nums = [2,3,1,2,4,3]
+target = 7
+
+left = 0
+current_sum = 0
+min_length = float('inf')
+
+for right in range(len(nums)):
+    current_sum += nums[right]
+    
+    while current_sum >= target:
+        min_length = min(min_length, right - left + 1)
+        current_sum -= nums[left]
+        left += 1
+
+if min_length == float('inf'):
+    print(0)
+else:
+    print("Minimum length:", min_length)
