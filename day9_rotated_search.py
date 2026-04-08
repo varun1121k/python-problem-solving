bcd171a9360d1542da956c902734f7d068df6a73
varun1121k <varun1121kknr@gmail.com>
2026-04-08 11:42:50 +0530

    Create day9_rotated_search.py

---
+++ day9_rotated_search.py
@@ -0,0 +1,24 @@
+# Day 9 - Search in Rotated Sorted Array
+
+nums = [4,5,6,7,0,1,2]
+target = 0
+
+left, right = 0, len(nums) - 1
+
+while left <= right:
+    mid = (left + right) // 2
+
+    if nums[mid] == target:
+        print("Found at index:", mid)
+        break
+
+    if nums[left] <= nums[mid]:
+        if nums[left] <= target < nums[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
+    else:
+        if nums[mid] < target <= nums[right]:
+            left = mid + 1
+        else:
+            right = mid - 1
