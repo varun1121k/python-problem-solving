2af488d92971786a0516bde357607f0bfccedc8b
varun1121k <varun1121kknr@gmail.com>
2026-04-10 23:17:45 +0530

    Create day10_lis.py

---
+++ day10_lis.py
@@ -0,0 +1,12 @@
+# Day 10 - Longest Increasing Subsequence
+
+nums = [10,9,2,5,3,7,101,18]
+
+dp = [1] * len(nums)
+
+for i in range(len(nums)):
+    for j in range(i):
+        if nums[i] > nums[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print("LIS length:", max(dp))
