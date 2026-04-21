81f941df565f6684308c8edf8d8a95489e6d86b1
varun1121k <varun1121kknr@gmail.com>
2026-04-21 10:40:12 +0530

    Create day13_spiral_matrix.py

---
+++ day13_spiral_matrix.py
@@ -0,0 +1,33 @@
+# Day 13 - Spiral Matrix
+
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+result = []
+
+top, bottom = 0, len(matrix) - 1
+left, right = 0, len(matrix[0]) - 1
+
+while top <= bottom and left <= right:
+    for i in range(left, right+1):
+        result.append(matrix[top][i])
+    top += 1
+
+    for i in range(top, bottom+1):
+        result.append(matrix[i][right])
+    right -= 1
+
+    if top <= bottom:
+        for i in range(right, left-1, -1):
+            result.append(matrix[bottom][i])
+        bottom -= 1
+
+    if left <= right:
+        for i in range(bottom, top-1, -1):
+            result.append(matrix[i][left])
+        left += 1
+
+print(result)
